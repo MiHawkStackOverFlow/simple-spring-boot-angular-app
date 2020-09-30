352ac3737f7e2c262759e40a63c3f747ff44f807
Abhishek Sharma <abhivsharma8@gmail.com>
2020-09-30 23:31:49 +0530

    ascii to number text

--- bike-ui/src/app/app.component.ts
+++ bike-ui/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './services/auth.service';
 import { myData } from './mock-data/data-neuro';
 import { Leaf } from './mock-data/leaf';
 import * as _ from 'lodash';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,10 @@ export class AppComponent implements OnInit {
   title = 'bike-ui';
   neuroData: Leaf[] = myData;
   childCounts: number = 0;
+  fileUploaded: any;
+  counter: number = 0;
+  currentLine: string = ``;
+  numbersData: string[] = [];
 
   constructor(private authService: AuthService) {
     authService.handleAuthentication();
@@ -79,4 +84,78 @@ export class AppComponent implements OnInit {
   }
 
 
+  /** Problem 3 */
+  get7segment(ascii) {
+      return ascii.
+          split('*').
+          reduce((r, a, i) => {
+              a.match(/.../g).forEach((b, j) => {
+                  r[j] = r[j] || [];
+                  r[j][i] = b;
+              });
+              return r;
+          }, []).
+          map((a) => {
+              return a.join('');
+          }).
+          map((a) => {
+              var bits = { 63: 0, 6: 1, 91: 2, 79: 3, 102: 4, 109: 5, 125: 6, 7: 7, 127: 8, 111: 9, 0: ' ' },
+                  v = '909561432'.split('').reduce((r, v: any, i) => {
+                      return r + (<any>(a[i] !== ' ') << v);
+                  }, 0);
+              return v in bits ? bits[v] : '*'; // * is an illegal character
+          }).
+          join('');
+  }
+
+  reset() { 
+    this.counter = 0;
+    this.currentLine = ``; 
+  }
+
+  concatLines(line) {
+    if(this.counter === 3) { 
+      this.currentLine = this.currentLine + line;
+    } else {
+      this.currentLine = this.currentLine + line + `*`;
+    }
+  }
+
+  createTextData(allLines) {
+    // Reading line by line
+    allLines.forEach((line) => {
+      this.counter = this.counter + 1;
+      if(line.length > 0) {
+        this.concatLines(line);
+      } else if(line.length === 0) {
+        if(this.currentLine !== "") {
+          this.numbersData.push(this.get7segment(this.currentLine));
+          this.numbersData.push('\n');
+        }
+        this.reset();  
+      }
+    });
+  }
+  
+  public onSelectFile(input) { // called each time file input changes
+    const file = input.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+        const file = <any>event.target.result;
+        const allLines = file.split(/\r\n|\n/);
+        this.createTextData(allLines);
+    };
+
+    reader.onerror = (event) => {
+      console.log('error', event.target.error.name);
+    };  
+
+    reader.readAsText(file);
+  }
+
+  download() {
+    var blob = new Blob(this.numbersData, {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "numbers.txt");
+  }
 }
